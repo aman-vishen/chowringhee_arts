a086b531186fe13d6ac1d543ef8f9e4cb6fcfabb
aman-vishen <amanvishen46@gmail.com>
2023-04-24 00:35:59 +0530

    added new images

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "batrapalak199@gmail.com";
+        $recipient = "info@chowringheearts.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
